fd0a073c55adf3ffbc28c90c9854c0413ceb7284
oneduck11034 <oneduck11034@gmail.com>
2024-10-24 20:55:46 +0900

    I have IQ 80

--- abc376/src/lib.rs
+++ abc376/src/lib.rs
@@ -1,20 +1,29 @@
 pub fn q_a(n: i64, c: i64, t_v: Vec<i64>) -> i64 {
     let mut result_candy= 0_i64;
-    let mut spand_time= 0_i64;
     let mut cnt= 0_usize;
+    let mut intervaled= 0_i64; 
+    let mut spend_time: Vec<i64>= Vec::new();
 
+    // 5
+    // first e -> 0s -> "1" ->(2) "3" ->(4) "7"
     for e_t in t_v {
         if cnt != 0 {
-            
-            if spand_time >= c {
-                result_candy+=1;
-                spand_time=0;
-            }
+            intervaled= e_t - intervaled;
+            spend_time.push(intervaled);
         }else if cnt == 0{
             result_candy+=1;
         }
-        
-        cnt+1;
+        cnt+=1;
+    }
+
+    let mut spend_time_b= spend_time.clone();
+    for i in 0..1 {
+        for j in 0..spend_time_b.len(){
+            let time= spend_time[i] +spend_time_b[j];
+            if time >= c {
+                result_candy+=1;
+            }
+        }
     }
 
     result_candy
